9588dbc241f6d5ba37277bfded632bf2bfef0889
holygwent <39002523+holygwent@users.noreply.github.com>
2022-04-12 21:14:40 +0200

    EF database
    EF database with Identity.
    added first migration initial

--- Examino/Server/Examino.API/Program.cs
+++ Examino/Server/Examino.API/Program.cs
@@ -1,8 +1,21 @@
+using Examino.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//sql connection
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Examino/Server/Examino.Database/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Database.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+   
+        public virtual Patient Patient { get; set; }
+     
+        public virtual Doctor Doctor { get; set; }
+    }
+}
