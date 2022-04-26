ab75937c3ec95c7261fbad2eaf368acfca15b658
holygwent <39002523+holygwent@users.noreply.github.com>
2022-04-26 12:16:52 +0200

    adding class DummyDoctors
    this class has function that gives you list of doctors

---
+++ Examino/Server/Examino.Infrastructure/DummyData/DummyDoctors.cs
@@ -0,0 +1,65 @@
+﻿using Examino.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Infrastructure.DummyData
+{
+    public class DummyDoctors
+    {
+        public static List<Doctor> GetPatients()
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            Doctor doctor1 = new Doctor()
+            {
+                Id = Guid.Parse("ae47d26c-b934-4057-8d05-dfd78ea1a138"),
+                Name = "Witold",
+                Surname = "Majewski",
+                PESEL = 66030280889,
+                BirthDay = new DateTime(1966, 3, 2),
+                Address = "Baciarego 10",
+                City = "Kraków",
+                Email = "WitoldMajewski@gmail.com",
+                UserName = "WitoldMajewski@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "999321933",
+                NormalizedEmail = "WITOLDMAJEWSKI@GMAIL.COM",
+                NormalizedUserName = "WITOLDMAJEWSKI@GMAIL.COM",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Qualification = "Gynaecologist",
+                Specialization = "obstetrics and gynaecology"
+            };
+            Doctor doctor2 = new Doctor()
+            {
+                Id = Guid.Parse("d9339e74-284d-46f4-ad46-b13269c4900e"),
+                Name = "Wojciech",
+                Surname = "Gwinciarz",
+                PESEL = 60070111229,
+                BirthDay = new DateTime(1960, 7, 1),
+                Address = "Bliska 20",
+                City = "Kraków",
+                Email = "WGwinciarz@gmail.com",
+                UserName = "WGwinciarz@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "129888777",
+                NormalizedEmail = "WGWINCIARZ@GMAIL.COM",
+                NormalizedUserName = "WGWINCIARZ@GMAIL.COM",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                Qualification= "Oncologist,laryngologist",
+                Specialization= "oncological radiotherapy,otolaryngology "
+
+            };
+
+
+            
+            doctors.Add(doctor1);
+            doctors.Add(doctor2);
+
+            return doctors;
+        }
+    }
+}
