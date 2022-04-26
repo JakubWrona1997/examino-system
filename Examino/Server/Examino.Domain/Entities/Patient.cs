48188a09dab7cb9a529ddfb74bbacaeedd333c3b
holygwent <39002523+holygwent@users.noreply.github.com>
2022-04-26 11:52:10 +0200

    ADDING DummyPatients
    class that gets list of exaple patients

--- Examino/Server/Examino.Domain/Entities/Patient.cs
+++ Examino/Server/Examino.Domain/Entities/Patient.cs
@@ -2,7 +2,7 @@
 {
     public class Patient : ApplicationUser
     {
-        public double? Height { get; set; }
+        public int? Height { get; set; }
         public int? Weight { get; set; }
         public string? BloodType { get; set; }
     }
---
+++ Examino/Server/Examino.Infrastructure/DummyData/DummyPatients.cs
@@ -0,0 +1,88 @@
+﻿using Examino.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Infrastructure.DummyData
+{
+    public  class DummyPatients
+    {
+        public static List<Patient> GetPatients()
+        {
+            List<Patient> patients = new List<Patient>(); 
+            Patient patient1 = new Patient()
+            {
+                Id = Guid.Parse("4d9b6996-8e8a-4cec-9f2f-d7f64e2113ca"),
+                Name = "Bartek",
+                Surname = "Skrzypek",
+                PESEL = 78032180802,
+                BirthDay = new DateTime(1978, 3, 21),
+                Address = "Kwiatowa 20",
+                City = "Jeleśnia",
+                Email = "BartekSkrzypek@gmail.com",
+                UserName = "BartekSkrzypek@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "999111333",
+                NormalizedEmail = "BARTEKSKRZYPEK@GMAIL.COM",
+                NormalizedUserName = "BARTEKSKRZYPEK@GMAIL.COM",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                BloodType = "B-",
+                Height = 175,
+                Weight = 80
+            };
+            Patient patient2 = new Patient()
+            {
+                Id = Guid.Parse("c394c3c5-3727-4b3b-999f-de8180edf15f"),
+                Name = "Michał",
+                Surname = "Gwizd",
+                PESEL = 90042182882,
+                BirthDay = new DateTime(1990, 4, 21),
+                Address = "Morksa 20",
+                City = "Gdańsk",
+                Email = "MichalGwizd@gmail.com",
+                UserName = "MichalGwizd@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "999888777",
+                NormalizedEmail = "MICHALGWIZD@GMAIL.COM",
+                NormalizedUserName = "MICHALGWIZD@GMAIL.COM",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                BloodType="B+",
+                Height=172,
+                Weight=70
+                
+            };
+            Patient patient3 = new Patient()
+            {
+                Id = Guid.Parse("f43c4920-a607-4d32-a937-ba324e07ebd6"),
+                Name = "Jakub",
+                Surname = "Kwiatowski",
+                PESEL = 99042382888,
+                BirthDay = new DateTime(1999, 4, 23),
+                Address = "Wiejska 50",
+                City = "Zakopane",
+                Email = "Jakub1999@gmail.com",
+                UserName = "Jakub1999@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "992488111",
+                NormalizedEmail = "JAKUB1999@GMAIL.COM",
+                NormalizedUserName = "JAKUB1999@GMAIL.COM",
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                BloodType = "A+",
+                Height = 192,
+                Weight = 90
+
+            };
+
+            patients.Add(patient1); 
+            patients.Add(patient2);
+            patients.Add(patient3);
+
+            return patients;
+        }
+    }
+}
