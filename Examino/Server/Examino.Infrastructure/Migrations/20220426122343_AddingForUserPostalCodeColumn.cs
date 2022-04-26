bf91356272bb031f07920bef610b95861af8ec40
holygwent <39002523+holygwent@users.noreply.github.com>
2022-04-26 15:05:09 +0200

    migration that adds postal code column

---
+++ Examino/Server/Examino.Infrastructure/Migrations/20220426122343_AddingForUserPostalCodeColumn.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Examino.Infrastructure.Migrations
+{
+    public partial class AddingForUserPostalCodeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Height",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Height",
+                table: "Users",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
