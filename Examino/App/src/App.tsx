e5f2d14e00e0ee43c4de614e9c9d42dc4580c37c
shirve <43320967+shirve@users.noreply.github.com>
2022-04-29 15:54:23 +0200

    routing, register page and login page

--- Examino/App/src/App.tsx
+++ Examino/App/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+
 function App() {
-  return <main>App</main>;
+  return (
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
