d61b467a4e3867eb165da6b342409ee43ae16b1c
holygwent <39002523+holygwent@users.noreply.github.com>
2022-05-14 17:42:13 +0200

    automapper

---
+++ Examino/Server/Examino.Application/Mapper/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Examino.Application.Functions.Registration.PatientRegistration;
+using Examino.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Mapper
+{
+    public class MappingProfiles :Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<RegisterPatientCommand, Patient>()
+                .ForMember(x => x.UserName, opt => opt.MapFrom(x => x.Email));
+        }
+    }
+}
