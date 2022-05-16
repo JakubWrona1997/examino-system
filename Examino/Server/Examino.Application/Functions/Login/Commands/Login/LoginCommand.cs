0a09ede93300205a33d5b36386950a8738dbce2b
holygwent <39002523+holygwent@users.noreply.github.com>
2022-05-16 15:25:06 +0200

    adding LoginCommand

---
+++ Examino/Server/Examino.Application/Functions/Login/Commands/Login/LoginCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Login.Commands.Login
+{
+    public  class LoginCommand:IRequest<LoginCommandResponse>
+    {
+        public string Email { get; set; }
+        public string  Password { get; set; }
+    }
+}
