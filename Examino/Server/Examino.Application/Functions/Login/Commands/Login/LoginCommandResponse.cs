2b4bb69dbb28798f29b486b90a6062a481c8d79e
holygwent <39002523+holygwent@users.noreply.github.com>
2022-05-16 18:19:07 +0200

    adding LoginCommandResponse

---
+++ Examino/Server/Examino.Application/Functions/Login/Commands/Login/LoginCommandResponse.cs
@@ -0,0 +1,30 @@
+﻿using Examino.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Login.Commands.Login
+{
+    public class LoginCommandResponse : BaseResponse
+    {
+
+        public string Token { get; set; }
+
+        public LoginCommandResponse(string token) : base()
+        {
+            Token = token;
+        }
+        public LoginCommandResponse(int statusCode, bool success) : base(statusCode, success)
+        {
+
+        }
+        public LoginCommandResponse(int statusCode, string message, bool success) : base(statusCode, message, success)
+        {
+
+        }
+
+    }
+
+}
