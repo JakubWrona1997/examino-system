527db1058edf23965e448b91de71ad5c386d5305
holygwent <39002523+holygwent@users.noreply.github.com>
2022-05-16 15:25:19 +0200

    adding LoginCommandValidator

---
+++ Examino/Server/Examino.Application/Functions/Login/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Login.Commands.Login
+{
+    public  class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty();
+                
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
