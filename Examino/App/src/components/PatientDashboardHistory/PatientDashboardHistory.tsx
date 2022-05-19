3719bc00e88ddc65662b2797b0f486488d925f32
shirve <43320967+shirve@users.noreply.github.com>
2022-05-19 22:03:31 +0200

    patient dashboard panel and history template

--- Examino/App/src/components/PatientDashboardHistory/PatientDashboardHistory.tsx
+++ Examino/App/src/components/PatientDashboardHistory/PatientDashboardHistory.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./PatientDashboardHistory.scss";
 
 const PatientDashboardHistory = () => {
-  return <div>Historia</div>;
+  return (
+    <React.Fragment>
+      <header className="dashboard-content-header">Historia</header>
+      <div className="dashboard-history-wrapper">
+        <ul className="card-labels">
+          <li>Nr</li>
+          <li>Doktor</li>
+          <li>Data wizyty</li>
+          <li>Recepta</li>
+        </ul>
+        <div className="card-wrapper">
+          <ul className="card-content">
+            <li>[index]</li>
+            <li>[doctor name]</li>
+            <li>[dd/mm/yyyy]</li>
+            <li>
+              <Link to={`/TODO`}>[receipt number]</Link>
+            </li>
+            <li>
+              <Link to={`/TODO`}>Pokaż szczegóły</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default PatientDashboardHistory;
--- Examino/App/src/components/PatientDashboardPanel/PatientDashboardPanel.tsx
+++ Examino/App/src/components/PatientDashboardPanel/PatientDashboardPanel.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./PatientDashboardPanel.scss";
 
 const PatientDashboardPanel = () => {
-  return <div>Panel</div>;
+  return (
+    <React.Fragment>
+      <header className="dashboard-content-header">Witaj [user]!</header>
+      <div className="dashboard-panel-wrapper">
+        <div className="card-wrapper">
+          <header className="card-header">Twoja ostatnia wizyta</header>
+          <div className="card-content">[content]</div>
+        </div>
+        <div className="card-wrapper">
+          <header className="card-header">Umów się na następną wizytę</header>
+          <div className="card-content">[content]</div>
+        </div>
+        <div className="card-wrapper">
+          <header className="card-header">Recepta</header>
+          <div className="card-content">[content]</div>
+        </div>
+        <div className="card-wrapper">
+          <header className="card-header">Skierowanie</header>
+          <div className="card-content">[content]</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default PatientDashboardPanel;
