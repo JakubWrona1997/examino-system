350943a4fbf3ac9b15ba57bd7e86d6dd4af50352
Jakub <jakub.wrona97@gmail.com>
2022-05-21 00:26:28 +0200

    Added UserProvider service

---
+++ Examino/Server/Examino.Infrastructure/Services/UserProvider.cs
@@ -0,0 +1,23 @@
+﻿using Examino.Domain.Contracts;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Infrastructure.Services
+{
+    public class UserProvider : IUserProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public Guid GetUserId() => Guid.Parse(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+       
+    }
+}
