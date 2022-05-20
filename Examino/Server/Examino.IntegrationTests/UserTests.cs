f809463dd177ed45c9e7e71526337cb490dc2936
Jakub <jakub.wrona97@gmail.com>
2022-05-21 00:26:56 +0200

    Added Testing Environment

---
+++ Examino/Server/Examino.IntegrationTests/UserTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Http;
+using Examino.Infrastructure;
+using Moq;
+using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Xunit;
+using Examino.Infrastructure.Services;
+using Examino.Application.Controllers;
+using MediatR;
+using Examino.Domain.Contracts;
+
+namespace Examino.IntegrationTests
+{
+    public class UserTests
+    {
+        private readonly Mock<IHttpContextAccessor> _httpContextAccessor;
+        private UserProvider _userProvider;
+
+        public UserTests()
+        {
+            _httpContextAccessor = new Mock<IHttpContextAccessor>();
+            _userProvider = new UserProvider(_httpContextAccessor.Object);
+        }
+
+        [Fact]
+        public async Task UserProvider_Should_Return_Logged_UserId()
+        {
+            //Arrange
+            var mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
+            string id = Guid.NewGuid().ToString();
+
+            _httpContextAccessor.Setup(x => x.HttpContext.User.FindFirst(It.IsAny<string>())).Returns(new Claim(ClaimTypes.NameIdentifier, id));
+            //Act
+            var result = _userProvider.GetUserId();
+            //Assert
+            Assert.Equal(id, result.ToString());
+        }
+    }
+}
