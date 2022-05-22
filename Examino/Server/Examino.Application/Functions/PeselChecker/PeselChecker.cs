411ea6ead0a35d88ec30f9a75a9eb451008c1ab6
Jakub <jakub.wrona97@gmail.com>
2022-05-23 00:47:10 +0200

    Added Pesel checker class

---
+++ Examino/Server/Examino.Application/Functions/PeselChecker/PeselChecker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.PeselChecker
+{
+    public class PeselChecker
+    {
+        private byte[] PESEL = new byte[11]; 
+        private bool isValid = false;
+
+        public PeselChecker(string peselNumber)
+        {
+            if(peselNumber.Length != 11)
+            {
+                isValid = false;
+            }
+            for (int i = 0; i < peselNumber.Length; i++)
+            {
+                PESEL[i] = byte.Parse(peselNumber.Substring(i, i + 1));
+            }
+            
+        }
+    }
+}
