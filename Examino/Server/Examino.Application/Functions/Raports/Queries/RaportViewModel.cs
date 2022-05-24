9dd224037f6bbba3b87212d2afa1d339de2d19d4
Jakub <jakub.wrona97@gmail.com>
2022-05-25 00:09:57 +0200

    Adjusting Dtos, created ViewModel

---
+++ Examino/Server/Examino.Application/Functions/Raports/Queries/RaportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Raports.Queries
+{
+    public class RaportViewModel
+    {
+        public string PatientName { get; set; }
+        public string PatientSurname { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorSurname { get; set; }
+        public RaportDto Raport { get; set; }
+        public PrescriptionDto Prescription { get; set; }
+    }
+}
