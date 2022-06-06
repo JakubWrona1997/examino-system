23f1df128018be5941576819ceba726823ad0094
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-06 13:59:54 +0200

    Create CreateDoctorCommand

---
+++ Examino/Server/Examino.Application/Functions/Users/Commands/CreateDoctor/CreateDoctorCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Users.Commands.CreateDoctor
+{
+    public class CreateDoctorCommand : IRequest<CreateDoctorCommandResponse>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PESEL { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
