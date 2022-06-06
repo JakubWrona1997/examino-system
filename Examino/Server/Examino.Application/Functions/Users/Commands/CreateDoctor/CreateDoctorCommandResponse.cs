c1700360246cef9b8b420236070a089040337811
holygwent <39002523+holygwent@users.noreply.github.com>
2022-06-06 14:00:17 +0200

    create CreateDoctorCommandResponse

---
+++ Examino/Server/Examino.Application/Functions/Users/Commands/CreateDoctor/CreateDoctorCommandResponse.cs
@@ -0,0 +1,29 @@
+﻿using Examino.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Users.Commands.CreateDoctor
+{
+    public class CreateDoctorCommandResponse : BaseResponse
+    {
+        public CreateDoctorCommandResponse() : base()
+        {
+
+        }
+        public CreateDoctorCommandResponse(string message) : base(message)
+        {
+
+        }
+        public CreateDoctorCommandResponse(int statusCode, bool success) : base(statusCode, success)
+        {
+
+        }
+        public CreateDoctorCommandResponse(int statusCode, string message, bool success) : base(statusCode, message, success)
+        {
+
+        }
+    }
+}
