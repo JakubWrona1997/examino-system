fa12f3638bbf7021ae9c6b6c35099cf2011e6db3
Jakub <jakub.wrona97@gmail.com>
2022-06-07 18:58:54 +0200

    Added DeleteDoctorCommand

---
+++ Examino/Server/Examino.Application/Functions/Users/Commands/DeleteDoctor/DeleteDoctorCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Examino.Domain.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examino.Application.Functions.Users.Commands.DeleteDoctor
+{
+    public class DeleteDoctorCommandHandler : IRequestHandler<DeleteDoctorCommand, Guid>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DeleteDoctorCommandHandler(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+        public async Task<Guid> Handle(DeleteDoctorCommand request, CancellationToken cancellationToken)
+        {
+            var doctorToDelete =  await _doctorRepository.GetDoctorById(request.DoctorId);
+
+            await _doctorRepository.DeleteDoctor(doctorToDelete);
+
+            return doctorToDelete.Id;
+        }
+    }
+}
