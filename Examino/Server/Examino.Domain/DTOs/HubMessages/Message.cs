286f384e4e37844d025b99e082e4b478b15c6bae
Jakub <jakub.wrona97@gmail.com>
2022-06-11 23:09:10 +0200

    Added message model

---
+++ Examino/Server/Examino.Domain/DTOs/HubMessages/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examino.Domain.DTOs
+{
+    public class Message
+    {
+        public string RaportId { get; set; }
+        public string ToUserId { get; set; }
+        public string FromUserId { get; set; }
+        public string TextMessage { get; set; }
+    }
+}
