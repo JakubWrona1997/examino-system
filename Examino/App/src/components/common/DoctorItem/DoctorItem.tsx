fba4bbe9d1778cc650432fe4355dadfca39c908d
shirve <43320967+shirve@users.noreply.github.com>
2022-06-12 16:11:49 +0200

    removed raport & doctor delete button

--- Examino/App/src/components/common/DoctorItem/DoctorItem.tsx
+++ Examino/App/src/components/common/DoctorItem/DoctorItem.tsx
@@ -1,8 +1,4 @@
-import React from "react";
-import { useAppDispatch } from "../../../app/store";
-import { deleteDoctor } from "../../../features/adminSlice";
 import { DoctorShortDetailsViewModel } from "../../../models/Users/Doctor/DoctorShortDetailsViewModel";
-import { FaTrash } from "react-icons/fa";
 import styles from "./DoctorItem.module.scss";
 
 interface Props {
@@ -11,33 +7,23 @@ interface Props {
 }
 
 const DoctorItem = ({ doctor, index }: Props) => {
-  const dispatch = useAppDispatch();
-
   return (
-    <React.Fragment>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <table>
-            <tbody>
-              <tr>
-                <td>{index}</td>
-                <td>
-                  {doctor.name}&nbsp;{doctor.surname}
-                </td>
-                <td>{doctor.pesel}</td>
-                <td>{doctor.qualification}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div
-          className={styles.content}
-          onClick={() => dispatch(deleteDoctor(doctor.id))}
-        >
-          <FaTrash />
-        </div>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <table>
+          <tbody>
+            <tr>
+              <td>{index}</td>
+              <td>
+                {doctor.name}&nbsp;{doctor.surname}
+              </td>
+              <td>{doctor.pesel}</td>
+              <td>{doctor.qualification}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
--- Examino/App/src/components/common/RaportItem/RaportItem.tsx
+++ Examino/App/src/components/common/RaportItem/RaportItem.tsx
@@ -1,10 +1,7 @@
-import React from "react";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { RootState, useAppDispatch } from "../../../app/store";
-import { deleteRaport } from "../../../features/raportSlice";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
 import { RaportViewModel } from "../../../models/Raports/RaportViewModel";
-import { FaTrash } from "react-icons/fa";
 import styles from "./RaportItem.module.scss";
 
 interface Props {
@@ -14,61 +11,48 @@ interface Props {
 
 const RaportItem = ({ raport, index }: Props) => {
   const { user } = useSelector((state: RootState) => state.user);
-  const dispatch = useAppDispatch();
 
   return (
-    <React.Fragment>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <table>
-            <tbody>
-              <tr>
-                <td>{index}</td>
-                {user?.role === "doctor" && (
-                  <td>
-                    {raport.patientName}&nbsp;{raport.patientSurname}
-                  </td>
-                )}
-                {user?.role === "patient" && (
-                  <td>
-                    {raport.doctorName}&nbsp;{raport.doctorSurname}
-                  </td>
-                )}
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <table>
+          <tbody>
+            <tr>
+              <td>{index}</td>
+              {user?.role === "doctor" && (
                 <td>
-                  {new Date(raport.raport.raportTime).toLocaleString("pl-PL")}
+                  {raport.patientName}&nbsp;{raport.patientSurname}
                 </td>
+              )}
+              {user?.role === "patient" && (
                 <td>
-                  {raport?.prescription ? (
-                    <Link
-                      to={`/${user?.role}/history/prescription/${raport.prescription.id}`}
-                    >
-                      Pokaż receptę
-                    </Link>
-                  ) : (
-                    "-"
-                  )}
+                  {raport.doctorName}&nbsp;{raport.doctorSurname}
                 </td>
-                <td>
+              )}
+              <td>
+                {new Date(raport.raport.raportTime).toLocaleString("pl-PL")}
+              </td>
+              <td>
+                {raport?.prescription ? (
                   <Link
-                    to={`/${user?.role}/history/raport/${raport.raport.id}`}
+                    to={`/${user?.role}/history/prescription/${raport.prescription.id}`}
                   >
-                    Pokaż szczegóły
+                    Pokaż receptę
                   </Link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        {user?.role === "doctor" && (
-          <div
-            className={styles.content}
-            onClick={() => dispatch(deleteRaport(raport.raport.id))}
-          >
-            <FaTrash />
-          </div>
-        )}
+                ) : (
+                  "-"
+                )}
+              </td>
+              <td>
+                <Link to={`/${user?.role}/history/raport/${raport.raport.id}`}>
+                  Pokaż szczegóły
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
