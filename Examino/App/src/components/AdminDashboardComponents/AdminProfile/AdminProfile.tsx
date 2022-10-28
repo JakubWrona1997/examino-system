3b1fa4bbc2fb5389804aa55356c5d283ac9a99fc
shirve <marekj1233@gmail.com>
2022-10-28 13:07:26 +0200

    feat: added alerts

--- Examino/App/src/components/AdminDashboardComponents/AdminProfile/AdminProfile.tsx
+++ Examino/App/src/components/AdminDashboardComponents/AdminProfile/AdminProfile.tsx
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AdminProfile.module.scss";
 import PasswordChangeModal from "../../Modals/PasswordChangeModal/PasswordChangeModal";
+import displayAlert from "../../../utils/displayAlert";
+import { useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "../../../app/store";
+import { removeAlert } from "../../../features/adminSlice";
 
 const AdminProfile = () => {
   const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false);
+
+  const { alert } = useSelector((state: RootState) => state.admin);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (alert) {
+      displayAlert(alert);
+    }
+    return () => {
+      if (alert) dispatch(removeAlert());
+    };
+  }, [alert]);
 
   const onPasswordModalOpen = () => setIsPasswordModalOpen((prev) => !prev);
 
